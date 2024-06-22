9e22fc3b44466d622674b0a51678c80f0476245a
AlineG18 <103582602+AlineG18@users.noreply.github.com>
2024-06-22 16:55:34 -0300

    Indentação
    Indentar código: Windows Shift + Alt + F

--- instalacao_vue/src/app.js
+++ instalacao_vue/src/app.js
@@ -1,18 +1,18 @@
 const MyNameApp = {
-    data(){
-        return {
-            name: "",
-            age: 30,
-            input_name:""
-        }
+  data() {
+    return {
+      name: "",
+      age: 30,
+      input_name: "",
+    };
+  },
+  methods: {
+    submirForm(e) {
+      e.preventDefault();
+      console.log(this.input_name);
+      this.name = this.input_name;
     },
-    methods: {
-        submirForm(e){
-            e.preventDefault();
-            console.log(this.input_name);
-            this.name = this.input_name
-        }
-    }
-}
+  },
+};
 
 Vue.createApp(MyNameApp).mount("#app");
