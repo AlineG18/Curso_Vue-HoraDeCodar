5d57fc2cddb9062a8584b5dccd6e490e5d2367c3
AlineG18 <103582602+AlineG18@users.noreply.github.com>
2024-06-28 11:02:11 -0300

    Aula_15-Múltiplos eventos

---
+++ siteweb_pedidosp1/src/components/MultiplosEventos.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="primeiro(txt1,$event),segundo($event),terceiro()">Ativar múltiplos eventos</button>
+    <p>{{ multiplos_eventos }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultiplosEventos",
+  data() {
+    return {
+      multiplos_eventos: "",
+      txt1: "Primeiro Evento"
+    }
+  },
+  methods:{
+    primeiro(txt1,e){
+        this.multiplos_eventos = txt1;
+     },
+    segundo(e){
+        setTimeout(()=> this.multiplos_eventos = "Segundo evento ativado!",900)
+    },
+    terceiro(){
+        setTimeout(()=> this.multiplos_eventos = "",1500)        
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  margin: 100px;
+}
+</style>
--- siteweb_pedidosp1/src/components/PrimeiroComponente.vue
+++ siteweb_pedidosp1/src/components/PrimeiroComponente.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <h1>Olá Vue!</h1>
-    <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
-  </div>  
-  </template>
-  
-  <script>
+    <p>Meu nome é: {{ nome }} e trabalho como {{ profissao }}</p>
+    <MultiplosEventos/>
+  </div>
+</template>
 
-  export default{
-    name:'PrimeiroComponente',    
-    data(){
-      return {
-        nome: "Aline",
-        profissao: "Programadora"
-      }
-    }       
+<script>
+import MultiplosEventos from './MultiplosEventos.vue';
+
+export default {
+  name: "PrimeiroComponente",
+  components:{
+    MultiplosEventos
+  },
+  data() {
+    return {
+      nome: "Aline",
+      profissao: "Desenvolvedora"
+    }
   }
-  </script>
+}
+</script>
